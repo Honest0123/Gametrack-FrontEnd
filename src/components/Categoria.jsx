ae61e904c490577cfc4f917ff044d99fab362132
termi5666 <juan.franco8b@gmail.com>
2025-11-12 20:46:04 -0500

    Hover de tarjeta de juego

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -5,9 +5,9 @@ export default function Categoria({ nombre }) {
     <div className="categoria">
       <h2>{nombre}</h2>
       <div className="lista-juegos">
-        <TarjetaDeJuego titulo="Juego 1" descripcion="Descripción del juego 1" calificacion={4} imagen="../assets/gamepad.png" />
-        <TarjetaDeJuego titulo="Juego 2" descripcion="Descripción del juego 2" calificacion={5} imagen="../assets/gamepad.png" />
-        <TarjetaDeJuego titulo="Juego 3" descripcion="Descripción del juego 3" calificacion={3} imagen="../assets/gamepad.png" />
+        <TarjetaDeJuego titulo="Juego 1" descripcion="Descripción del juego 1" calificacion={4} imagen="public/vite.svg" />
+        <TarjetaDeJuego titulo="Juego 2" descripcion="Descripción del juego 2" calificacion={5} imagen="public/vite.svg" />
+        <TarjetaDeJuego titulo="Juego 3" descripcion="Descripción del juego 3" calificacion={3} imagen="public/vite.svg" />
       </div>
     </div>
   )
--- src/components/TarjetaDeJuego.jsx
+++ src/components/TarjetaDeJuego.jsx
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import { Icon } from "@iconify/react";
 
 export default function TarjetaDeJuego({ titulo, descripcion, calificacion, imagen }) {
+  const [preview, setPreview] = useState(false);
 
   const estrellas = Array.from({ length: 5 }, (_, i) => (
     <Icon key={i} icon={i < calificacion ? "icon-park-solid:star" : "icon-park-outline:star"} />
   ));
 
   return (
-    <div className="tarjeta-juego">
-        <img src="public/vite.svg" alt={"imagen de " + titulo} style={{ width: '100px', height: '100px' }}/>
-        <h3>{titulo}</h3>
-        <p>{descripcion}</p>
-        <div className="calificacion">{estrellas}</div>
-    </div>
+    <div
+      className={`tarjeta-juego ${preview ? "preview" : ""}`}
+      onMouseEnter={() => setPreview(true)}
+      onMouseLeave={() => setPreview(false)}
+      >
+
+      {!preview ? (
+        <>
+          <img src={imagen} alt={"imagen de " + titulo} className="imagen-juego"/>
+          <h3 className="titulo-juego">{titulo}</h3>
+          <p className="descripcion-juego">{descripcion}</p>
+          <div className="calificacion">{estrellas}</div>
+        </>
+        
+      ) : (
+        <>
+          <div className="info-primera">
+            <h3 className="titulo-juego">{titulo}</h3>
+            <div className="calificacion">{estrellas}</div>
+          </div>
+          <div className="info-segunda">
+            <p className="fecha-lanzamiento">Lanzamiento: 2023</p>
+            <p className="genero-juego">Género: Acción</p>
+          </div>
+          <div className="info-imagenes">
+            <img src={imagen} alt={"imagen de " + titulo} className="imagen-1"/>
+            <img src={imagen} alt={"imagen de " + titulo} className="imagen-2"/>
+            <img src={imagen} alt={"imagen de " + titulo} className="imagen-3"/>
+          </div>
+          <p className="descripcion-juego">{descripcion}</p> <br/>
+          <p className="plataformas-juego">Plataformas: PC, PS5, Xbox</p>
+        </>
+      )} 
+
+    </div>    
+
   )
 }
