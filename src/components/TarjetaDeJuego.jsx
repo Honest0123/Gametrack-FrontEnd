20bb5c6f82765ea510719bd302a767ff44356d1f
termi5666 <juan.franco8b@gmail.com>
2025-11-18 12:38:26 -0500

    Fecha de lanzamiento y Plataforma del backend

--- src/components/TarjetaDeJuego.jsx
+++ src/components/TarjetaDeJuego.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Icon } from "@iconify/react";
+import gamepadLogo from '../assets/gamepad.png'
 
 export default function TarjetaDeJuego({ data }) {
   const [preview, setPreview] = useState(false);
+
+  /* useEffect(() => {
+    // calcular calificacion en base a las reviews del juego si es que existen
+    try {
+      const res = fetch(`${import.meta.env.VITE_API_URL}/api/Games/reviews?juegoId=${data._id}`);
+    }
+  }); */
 
   const calificacion = data.calificacion || 0;
   const estrellas = Array.from({ length: 5 }, (_, i) => (
@@ -15,9 +23,9 @@ export default function TarjetaDeJuego({ data }) {
   const imagenPortada = data.imagenPortada || "/vite.svg";
   const imagen2 = data.imagen2 || "/vite.svg";
   const imagen3 = data.imagen3 || "/vite.svg";
-  const fechaLanzamiento = data.fechaLanzamiento || "2023-01-01";
+  const fechaLanzamiento = data.añoLanzamiento || "2023-01-01";
   const genero = data.genero || "Acción";
-  const plataformas = data.plataformas || ["PC", "PS5", "Xbox"];
+  const plataformas = data.plataforma || ["PC", "PS5", "Xbox"];
 
   return (
     <div
@@ -41,8 +49,8 @@ export default function TarjetaDeJuego({ data }) {
             <div className="calificacion">{estrellas}</div>
           </div>
           <div className="info-segunda">
-            <p className="fecha-lanzamiento">Lanzamiento: {fechaLanzamiento}</p>
-            <p className="genero-juego">Género: {genero}</p>
+            <p className="fecha-lanzamiento">Lanzamiento: {fechaLanzamiento.split("T")[0]}</p>
+            <p className="genero-juego">{genero}</p>
           </div>
           <div className="info-imagenes">
             <img src={imagenPortada} alt={"imagen de " + titulo} className="imagen-1"/>
@@ -50,7 +58,7 @@ export default function TarjetaDeJuego({ data }) {
             <img src={imagen3} alt={"imagen de " + titulo} className="imagen-3"/>
           </div>
           <p className="descripcion-juego-preview">{descripcion}</p> <br/>
-          <p className="plataformas-juego">Plataformas: {plataformas.join(", ")}</p>
+          <p className="plataformas-juego">{plataformas}</p>
         </>
       )} 
 
