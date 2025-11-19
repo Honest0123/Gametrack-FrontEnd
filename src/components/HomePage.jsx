216b17defb2edda778c84588d4a58d5671278cbd
termi5666 <juan.franco8b@gmail.com>
2025-11-19 16:14:37 -0500

    Add HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,28 @@
+import '../styles/HomePage.css'
+import VideoMenu from "/Menu.mp4"
+import gamepadLogo from '../assets/gamepad.png'
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Gametrack home page!</p>
+    <div className ='Home'>
+      <div className='Video'>
+        <video autoPlay loop muted playsInline className='Backgrund'>
+        <source src={VideoMenu} type="video/mp4"/>
+        </video>
+      </div>
+      
+      <div className='Logo'>
+        <img src={gamepadLogo} id="imgLogo"></img>
+      </div>
+
+      <div className='Title'>
+        <h1>GameTrack</h1>
+        <p>Welcome to the Gametrack home page!</p>
+      </div>
+      
+
+
+    
     </div>
+
   )
 }
